688cc6e72867b73abac96fab0a5ea2bc646add97
Wael Tarabishi <wealtarabishinext@gmail.com>
2024-12-25 18:30:51 +0300

    Add Product model to Prisma and update build script for deployment ⛏🚀👚👚

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,10 +1,5 @@
-import { auth } from "@/auth";
-import React from "react";
-import { currentUserRole } from "../../../../lib/auth";
-
 const AdminPage = async () => {
-  const usr = await currentUserRole();
-  return <>{usr}</>;
+  return <></>;
 };
 
 export default AdminPage;
